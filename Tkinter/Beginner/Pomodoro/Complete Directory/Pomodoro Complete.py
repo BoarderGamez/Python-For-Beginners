a2a084330d34bfc89bc2487b8a7e30ae48605fba
BoarderGamez <115997081+BoarderGamez@users.noreply.github.com>
2025-08-13 21:22:59 +0100

    Started working on the first Beginner program Pomodoro Complete.py

---
+++ Tkinter/Beginner/Pomodoro/Complete Directory/Pomodoro Complete.py
@@ -0,0 +1,42 @@
+from tkinter import *
+root = Tk()
+root.iconbitmap("Tomato.ico")
+root.title("Pomodoro Timer")
+# Function
+
+def timer(time):
+    global Stop
+    if time >= 0:
+        mins = time //60
+        secs = time % 60
+        TimerLabel.config(text =f"{mins}:{secs}" )
+        if Stop == False:
+           root.after(1000, timer, time-1)
+        else:
+            Stop=False
+            TimerLabel.config(text = "00:00")
+            return
+
+    else:
+        TimerLabel.config(text = "00:00")
+def stop():
+    global Stop
+    Stop = True
+# Variables
+Stop = False
+# Frames
+TimerFrame = Frame(root)
+TimerFrame.pack(padx= 10, pady= 10)
+
+ButtonFrame = Frame(root)
+ButtonFrame.pack(padx= 10,pady= 10)
+# Labels
+TimerLabel = Label(TimerFrame, text = "00:00", font = ("Arial",40, "bold"))
+TimerLabel.grid(column= 0, columnspan = 3,padx= 20,pady= 20)
+# Button
+StartButton = Button(ButtonFrame,text = "Start", font = ("Arial",15),bg ="Light Green",command=lambda: timer(1500))
+StartButton.grid(column = 0, row= 0, padx=5)
+
+StopButton = Button(ButtonFrame, text = "Reset", font = ("Arial",15),bg= "Red",command=stop)
+StopButton.grid(column= 2, row= 0, padx= 5)
+root.mainloop()
