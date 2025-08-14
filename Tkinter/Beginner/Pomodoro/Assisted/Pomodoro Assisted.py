24aca99b641cc93a63efc455ae8f87deb2547c28
BoarderGamez <115997081+BoarderGamez@users.noreply.github.com>
2025-08-14 12:07:05 +0100

    Added some files and finished pomodoro

---
+++ Tkinter/Beginner/Pomodoro/Assisted/Pomodoro Assisted.py
@@ -0,0 +1,29 @@
+from tkinter import *  # It is import * not import as Tk to make it more simple for example instead of Tk.label, here you write just label
+
+# Initialization of the Tkinter window
+root = Tk()
+root.title("Pomodoro Timer")  # Naming the title
+# You can add the Tomato.ico if you want
+
+# Variables
+Stop = False
+
+# Functions
+
+
+def timer(time):
+    pass  # You can implement the countdown logic
+# Function For Stop Button
+
+
+def stop():  # This happens when the stop button is pressed
+    pass
+
+# Frames (Optional)
+
+# Labels Create the timer button
+
+# Buttons Create the start, stop, small break, and big break buttons
+
+
+root.mainloop()
