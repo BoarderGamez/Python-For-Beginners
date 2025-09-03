92bb0c4d56af84c2f8f890133251f943de77f083
board <boardergamezdiscord@gmail.com>
2025-09-03 19:22:07 +0200

    removed exe not working got the pomodor complete working with both buttons same time

--- Program/Intermediate/Pomodoro/Complete/Pomodoro Complete.py
+++ Program/Intermediate/Pomodoro/Complete/Pomodoro Complete.py
@@ -5,23 +5,30 @@ root = Tk()
 root.iconbitmap("Tomato.ico") # Window Icon
 root.title("Pomodoro Timer") # Naming the title
 
+IsTimer = False
 # Functions
-def timer(time):
-    global Stop # You require this because of line 18, without this it would be thought that the Stop variable is local.
-    if time >= 0: # While the timer is running
-        mins = time // 60
-        secs = time % 60
-        TimerLabel.config(text=f"{mins}:{secs}") # Config, to change the timer
-        if not Stop: # If the stop button has not been pressed.
-            root.after(1000, timer, time - 1) # After 1000ms (1s) run timer again and count down a second
-        else: # If stop has been set to true
-            Stop = False # Set the stop variable to false
-            TimerLabel.config(text="00:00") # Reset the timer
-            return # Exit the function
+def TimerCmd(time):
+    global IsTimer
+    if not IsTimer:
+        def timer(t):
+            global Stop, IsTimer # You require this because of line 18, without this it would be thought that the Stop variable is local.
+            if t >= 0: # While the timer is running
+                IsTimer = True
+                mins = t // 60
+                secs = t % 60
+                TimerLabel.config(text=f"{mins:02}:{secs:02}") # Config, to change the timer
+                if not Stop: # If the stop button has not been pressed.
+                    root.after(1000, timer, t - 1) # After 1000ms (1s) run timer again and count down a second
+                else: # If stop has been set to true
+                    Stop = False # Set the stop variable to false
+                    IsTimer = False
+                    TimerLabel.config(text="00:00") # Reset the timer
+                    return # Exit the function
 
-    else: # If the timer is done, reset the clock
-        TimerLabel.config(text="00:00")
-
+            else: # If the timer is done, reset the clock
+                IsTimer = False
+                TimerLabel.config(text="00:00")
+        timer(time)
 # Function For Stop Button
 def stop(): # This happens when the stop button is pressed
     global Stop
@@ -43,7 +50,7 @@ TimerLabel.grid(column=0, columnspan=4, padx=20, pady=20) # Columnspan is to str
 
 # Buttons
 StartButton = Button(ButtonFrame, text="Start", font=(
-    "Arial", 15), bg="Light Green", command=lambda: timer(1500)) # Lambda is there so that a function with a parameter can be used without calling the function
+    "Arial", 15), bg="Light Green", command=lambda: TimerCmd(1500)) # Lambda is there so that a function with a parameter can be used without calling the function
 StartButton.grid(column=0, row=0, padx=5)
 
 StopButton = Button(ButtonFrame, text="Reset", font=(
@@ -51,11 +58,11 @@ StopButton = Button(ButtonFrame, text="Reset", font=(
 StopButton.grid(column=3, row=0, padx=5)
 
 SmallBreakButton = Button(ButtonFrame, text="Small Break", font=(
-    "Arial", 15), bg="Light Blue", command=lambda: timer(300)) # Lambda is there so that a function with a parameter can be used without calling the function
+    "Arial", 15), bg="Light Blue", command=lambda: TimerCmd(300)) # Lambda is there so that a function with a parameter can be used without calling the function
 SmallBreakButton.grid(column=1, row=0, padx=5)
 
 BigBreakButton = Button(ButtonFrame, text="Big Break", font=(
-    "Arial", 15), bg="Orange", command=lambda: timer(900)) # Lambda is there so that a function with a parameter can be used without calling the function
+    "Arial", 15), bg="Orange", command=lambda: TimerCmd(900)) # Lambda is there so that a function with a parameter can be used without calling the function
 BigBreakButton.grid(column=2, row=0, padx=5)
 
 root.mainloop()
